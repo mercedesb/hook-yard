3d844f04256d2acb1cddf97a7635e0864ed5b3c7
mercedesb <mercedesbernard@live.com>
2018-02-11 13:42:05 -0600

    got Apollo 1.4.16 working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { gql, graphql, ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: '/',
+  })
+});
+
+const graphQLQuery = gql`
+  {
+    patterns {
+      title
+    }
+  }
+`
+
+const getGraphQLEnhancedComponent = graphql(graphQLQuery);
+
+const DataViewer = ({ data: {loading, error, patterns }}) => {
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div>
+      <ul>{ patterns.map( a => <li key={a.title}>{a.title}</li>) }</ul>
+    </div>
+  )
+};
+
+const DataViewerWithData = getGraphQLEnhancedComponent(DataViewer);
 
 class App extends Component {
   state = {
@@ -25,15 +56,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.response}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <ApolloProvider client={client}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">{this.state.response}</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+
+              <DataViewerWithData/>
+            </p>
+          </div>
+        </ApolloProvider>
+
     );
   }
 }
